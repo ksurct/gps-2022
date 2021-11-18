285f0be79d7114d675f34eaf893d18660e6c40f9
Dylan Staatz <dms9800@gmail.com>
2021-11-17 18:57:43 -0600

    11-17

--- ultrasonicSensorCode/testFunction.ino
+++ ultrasonicSensorCode/testFunction.ino
@@ -1,33 +1,56 @@
 
 
-
-
 struct SensorData {
-   int left;
-   int leftAngle;
-   int center;
-   int rightAngle;
-   int right;
+   int fr_data;
+   int fl_data;
+   int f_data;
+   int l_data;
+   int r_data;
 };
+void setup(){
 
-SensorData sensorDataFunction(int echoPin, int trigPin, int sizeOfDistances){
-SensorData data;
+/*
+On current board
+fr (front-right) = TOF1
+fl (front-left) = TOF4
+f (front) = TOF5
+l (left) = TOF3
+r (right) = TOF2
+*/
+#define echoPinF1 32 
+#define trigPinF1 31 
+#define echoPinF2 30
+#define trigPinF2 29
+#define echoPinF3 38
+#define trigPinF3 37
+#define echoPinF4 36
+#define trigPinF4 35
+#define echoPinF5 34
+#define trigPinF5 33
+
+//sets the trig pins as outputs
+pinMode(trigPinF1, OUTPUT);
+pinMode(trigPinF2, OUTPUT);
+pinMode(trigPinF3, OUTPUT);
+pinMode(trigPinF4, OUTPUT);
+pinMode(trigPinF5, OUTPUT);
+}
+int sensorDataFunction(int sizeOfDistances, int epin, int tpin){
+
 long duration1; // variable for the duration of sound wave travel
 int distance1; // variable for the distance measurement
-#define echoPin1 echoPin // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin1 trigPin  //attach pin D3 Arduino to pin Trig of HC-SR04
-pinMode(trigPin1, OUTPUT); // Sets the trigPin as an OUTPUT
+
  int distance[sizeOfDistances];
   for(int i = 0; i < sizeOfDistances; i++){
-  digitalWrite(trigPin1, LOW);
+  digitalWrite(tpin, LOW);
   delayMicroseconds(2);
   // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-  digitalWrite(trigPin1, HIGH);
+  digitalWrite(tpin, HIGH);
   delayMicroseconds(1);
-  digitalWrite(trigPin1, LOW);
+  digitalWrite(tpin, LOW);
   // Reads the echoPin, returns the sound wave travel time in microseconds
   // Calculating the distance
-  distance[i] = pulseIn(echoPin1, HIGH) * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  distance[i] = pulseIn(epin, HIGH) * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
   // Displays the distance on the Serial Monitor
   }
   int average = 0;
@@ -40,17 +63,24 @@ pinMode(trigPin1, OUTPUT); // Sets the trigPin as an OUTPUT
   }
   if(elements != 0){
     average = average / elements;
-    data.left = average;
+    return average;
   }else{
-
-    
     return -1;
   }
-  
+}
 
+SensorData collectData(int sizeOfDistances){
+  SensorData data;
+  data.fr_data = sensorDataFunction(sizeOfDistances, echoPinF1, trigPinF1);
+  data.r_data = sensorDataFunction(sizeOfDistances, echoPinF2, trigPinF2);
+  data.l_data = sensorDataFunction(sizeOfDistances, echoPinF3, trigPinF3);
+  data.fl_data = sensorDataFunction(sizeOfDistances, echoPinF4, trigPinF4);
+  data.f_data = sensorDataFunction(sizeOfDistances, echoPinF5, trigPinF5);
+  return data;
+}
 
-    
- 
-
-  
+void loop(){
+SensorData data = collectData(5);
+Serial.print("Test Distance: ");
+Serial.print(data.l_data);
 }
