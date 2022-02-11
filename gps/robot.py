e558040c9e60b896bdd7859eaeb88d55dcc4576c
Alex Curchin <alexcurchin@ip-10-150-53-39.wireless.ksu.edu>
2022-02-10 19:02:30 -0600

    added simple input

--- gps/robot.py
+++ gps/robot.py
@@ -1,8 +1,8 @@
-from motor import Motor
+#from motor import Motor
 
 class Robot():
     def __init__(self):
-        pass
+        self.stopped = True
 
     # Compass
     def getAngle(self):
@@ -31,23 +31,28 @@ class Robot():
     
     # Set constant speed
     def constantMove(self, speed):
-        pass
+        print("Moving")
+        self.stopped = False
 
     # Set constant rotation
     def constantRotate(self, speed):
-        pass
+        print("Rotating")
+        self.stopped = False
 
     # Move a certain distance at a speed
     def move(self, speed, distance):
-        pass
+        print("Move " + distance + " at " + speed)
+        self.stopped = False
 
     # Rotate a certain amount at a certain speed
     def rotate(self, speed, degrees):
-        pass
+        print("Rotate " + degrees + " at " + speed)
+        self.stopped = False
 
     # Stop the robot
     def stop(self):
-        pass
+        print("Stop")
+        self.stopped = True
 
 class Camera():
     def __init__(self):
