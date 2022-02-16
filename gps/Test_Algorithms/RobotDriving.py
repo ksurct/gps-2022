cf2aecbc563bab38db0ee8ea6641549dc77aecd0
Robotics Lab Comp 1 <you@example.com>
2022-02-15 19:37:54 -0600

    Ignore this

--- gps/Test_Algorithms/RobotDriving.py
+++ gps/Test_Algorithms/RobotDriving.py
@@ -1,3 +1,4 @@
+from pickle import TRUE
 import robot_sim
 import random
 import pygame
@@ -13,6 +14,7 @@ yellow = (255, 255 , 0, 255)
 mod = 0
 flag = 0
 is_blue = False
+
 
 
 # Algorithm
@@ -41,41 +43,53 @@ def algorithm(robot, time, events):
     #for sensorName in sensorData:
         #print(sensorName, 'distance =', sensorData[sensorName])
 
-    for cameraName in cameraData.keys():
-        camera = cameraData[cameraName]
-        # Do something
+
+
+    camera = cameraData['main']
+    # Do something
+    
+    #robot.constantMove(60)
+    FoundBlue = False
+    for split in camera:
         
-        for split in camera:
+        print("[", end="")
+        for object in split:
+            if (object['color'] == blue):
+                print("(blue,{}),".format(object['size']), end="")
+                # FoundBlue = True
+            # elif (object['color'] != blue):
+            #     FoundBlue = False
             
-            #print("[", end="")
-            for object in split:
-                '''
-                if (object == blue):
-                    print("blue,", end="")
-                elif (object == white):
-                    print("white,", end="")
-                elif (object == green):
-                    print("green,", end="")
-                elif (object == red):
-                    print("red,", end="")
-                '''
-
-            #print("]")
-            print(split)
-
-        if blue in camera[2] or blue in camera[1]:
-            robot.rotate(30,10)
-        elif blue in camera[0]:
-            is_blue = True
-            robot.move(30,30)
-        elif is_blue:
-            robot.rotate(-30,10)
-        else:
-            robot.move(30,30)
-
-            
+            # elif (object['color'] == white):
+            #     print("(white,{}),".format(object['size']), end="")
+            # elif (object['color'] == green):
+            #     print("(green,{}),".format(object['size']), end="")
+            # elif (object['color'] == red):
+            #     print("(red,{}),".format(object['size']), end="")
+        print("]")
 
 
+
+    #if blue in camera[2] or blue in camera[1]:
+    #     robot.rotate(300,35)
+    #elif blue in camera[0]:
+    #     is_blue = True
+    #     robot.move(300, 35)
+    #elif is_blue:
+    #     robot.rotate(-300,35)
+    #else:
+         #robot.move(300,35)
+
+        
+
+    # robot.constantMove(30)
+
+    # if FoundBlue == True:
+    #     print("True")
+    # if FoundBlue == False:
+    #     print("False")
+    
+    
         
         #if(robot.getAngle() < 180):
             #robot.rotate(120, 182-robot.getAngle())
@@ -86,13 +100,20 @@ def algorithm(robot, time, events):
 
         
     
-    # if (sensorData['Front'] < 100 and sensorData['Front'] != -1):
-    #     while (mod == 0):
-    #         mod = random.randrange(-1,2)
-    #     robot.rotate(700 * mod, 35)
-    # elif (robot.isNotMoving()):
-    #     mod = 0
-    #     robot.constantMove(400)
+    if (sensorData['Front'] < 100 and sensorData['Front'] != -1):
+        if (white in camera[1] or white in camera[0]):
+            robot.constantMove(-50)
+            robot.constantRotate(300)
+            robot.rotate(300, 35)
+    elif (sensorData['TL'] < 3000 and sensorData['TL'] != -1):
+        if (blue in camera[1] or blue in camera[0]):
+            robot.rotate(-300,35)
+        #while (mod == 0):
+        #    mod = random.randrange(-1,2)
+        #robot.rotate(700 * mod, 35)
+    elif (robot.isNotMoving()):
+        mod = 0
+        robot.constantMove(400)
 
 def keyboard(events):
     for event in events:
@@ -152,10 +173,10 @@ course.circle(x =107,y=120,r=2,c=yellow)
 #      (0,0)x----------
 #
 sensors = {
-    "TL": robot_sim.Sensor(x=0,y=50,d=300,angle=-90,debug=False),
-    "BL": robot_sim.Sensor(x=0,y=0,d=300,angle=-90,debug=False),
-    "BR": robot_sim.Sensor(x=25,y=0,d=300,angle=90,debug=False),
-    "TR": robot_sim.Sensor(x=25,y=50,d=300,angle=90,debug=False),
+    "TL": robot_sim.Sensor(x=0,y=50,d=300,angle=-90,debug=True),
+    "BL": robot_sim.Sensor(x=0,y=0,d=300,angle=-90,debug=True),
+    "BR": robot_sim.Sensor(x=25,y=0,d=300,angle=90,debug=True),
+    "TR": robot_sim.Sensor(x=25,y=50,d=300,angle=90,debug=True),
     "Front": robot_sim.Sensor(x=4.5,y=9.5,d=300,angle=0,debug=True)
 }
 
@@ -179,6 +200,7 @@ cameras = {
                    fieldOfView=90,
                    splitCount=3, # How many splits are in the camera when showing object colors
                    resolution=40, # How many rays are in the field of view
+                   maxDistance=300,
                    debug=True
                    )
 }
