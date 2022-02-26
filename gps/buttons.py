c9e254acb2dc6443b81c932a8cbfabe08c453465
Your Name <you@example.com>
2022-02-25 19:22:31 -0600

    button stuff

--- gps/buttons.py
+++ gps/buttons.py
@@ -11,6 +11,9 @@ class Button:
     def __init__(self, pin, func):
         self.pin = pin
         self.func = func
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(pin, GPIO.RISING, 
+            callback=func, bouncetime=100)
         
     def getPin(self):
         return self.pin
@@ -43,36 +46,23 @@ def signal_handler(sig, frame):
     sys.exit(0)
         
 if __name__ == "__main__":
+    
+    GPIO.setmode(GPIO.BCM)
     b1 = Button(11, f) 
     b2 = Button(25, f) 
-    b3 = Button(8, f) 
+    b3 = Button(8, reboot) 
     b4 = Button(7, f)
     led1 = 16 # green
     led2 = 20 # red
     led3 = 21 # the weird orange one
     led4 = 18 # white
     
-    GPIO.setmode(GPIO.BCM)
     
     #LEDs
     GPIO.setup(led1, GPIO.OUT)
     GPIO.setup(led2, GPIO.OUT)
     GPIO.setup(led3, GPIO.OUT)
     GPIO.setup(led4, GPIO.OUT)
-    
-    # Buttons
-    GPIO.setup(b1.getPin(), GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(b2.getPin(), GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(b3.getPin(), GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(b4.getPin(), GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(b1.getPin(), GPIO.RISING, 
-            callback=b1.callFunc, bouncetime=100)
-    GPIO.add_event_detect(b2.getPin(), GPIO.RISING, 
-            callback=b2.callFunc, bouncetime=100)
-    GPIO.add_event_detect(b3.getPin(), GPIO.RISING, 
-            callback=reboot, bouncetime=100)
-    GPIO.add_event_detect(b4.getPin(), GPIO.RISING, 
-            callback=b4.callFunc, bouncetime=100)
     
     signal.signal(signal.SIGINT, signal_handler)
     signal.pause()
