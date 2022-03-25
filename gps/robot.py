ed0748f0ff1b7498a5d715a1bffaaaa4208aeda0
Dylan Staatz <dms9800@gmail.com>
2022-03-24 20:17:31 -0500

    sim with roomba works

--- gps/robot.py
+++ gps/robot.py
@@ -144,8 +144,8 @@ class Robot():
     # Set constant rotation
     def constantRotate(self, speedDps):
         speedPercent = self.dpsToPercent(speedDps)
-        self.right.setSpeed(-speedPercent*self.rightTurnMod)
-        self.left.setSpeed(speedPercent*self.leftTurnMod)
+        self.right.setSpeed(speedPercent*self.rightTurnMod)
+        self.left.setSpeed(-speedPercent*self.leftTurnMod)
         self.timeCalled = time.time()
         self.timeToKill = 0
         self.constant = True
@@ -172,7 +172,7 @@ class Robot():
         # print(speedDps)
         # print(-speedDps)
         self.right.setSpeed(speedPercent)
-        self.left.setSpeed(-speedPercent)
+        self.left.setSpeed(speedPercent)
         self.timeCalled = time.time()
         self.timeToKill = seconds
         self.constant = False
