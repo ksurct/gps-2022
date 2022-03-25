4e1d859d124f3fad6831bb6d77de12cd5d728310
Your Name <you@example.com>
2022-03-24 21:01:43 -0500

    more stuff :)

--- gps/serialTeensyToPi.py
+++ gps/serialTeensyToPi.py
@@ -7,27 +7,27 @@ class SerialInput(object):
     def __init__(self):
         self.ser = serial.Serial(port='/dev/ttyACM0', baudrate=115200)
         self.ser.flush()
-        self.fr_data = ""
-        self.fl_data = ""
-        self.f_data = ""
-        self.l_data = ""
-        self.r_data = ""
-        self.course = ""
-        self.longitude = ""
-        self.latitude = ""
-        self.altitude = ""
-        self.speed = ""
-        self.accelX= ""
-        self.accelY= ""
-        self.accelZ= ""
-        self.magX = ""
-        self.magY = ""
-        self.magZ = ""
+        self.fr_data = -1
+        self.fl_data = -1
+        self.f_data = -1
+        self.l_data = -1
+        self.r_data = -1
+        self.course = 0.0
+        self.longitude = 0.0
+        self.latitude = 0.0
+        self.altitude = 0.0
+        self.speed = 0.0
+        self.accelX= 0.0
+        self.accelY= 0.0
+        self.accelZ= 0.0
+        self.magX = 0.0
+        self.magY = 0.0
+        self.magZ = 0.0
 
     def receiveData(self):
         self.pingTeensy()
         line = json.loads(self.ser.readline().decode('utf-8').rstrip())
-        # print(line)
+        print(line)
         self.fr_data = line['fr_data']
         self.fl_data = line['fl_data']
         self.f_data = line['f_data']
