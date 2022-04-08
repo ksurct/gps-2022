73aca12c9b7e5cddb59ae543abb1e060c944f255
Caden Churchman <ccaden21@gmail.com>
2022-04-07 22:27:03 -0500

    Added start

---
+++ gps/start.py
@@ -0,0 +1,21 @@
+
+
+from buttons import Button
+
+def normal():
+    print("Normal")
+
+def fast():
+    print("fast")
+
+def terminal():
+    print("terminal")
+
+def escapeVelocity():
+    print("escapeVelocity")
+
+b1 = Button(11, normal) 
+b2 = Button(25, fast) 
+b3 = Button(8, terminal) 
+b4 = Button(7, escapeVelocity)
+
